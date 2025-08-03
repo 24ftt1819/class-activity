14e9441ff4f4d5ae8246a5ce44c5305ec935f5ec
24ftt1819 <24FTT1819@student.pb.edu.bn>
2025-08-04 00:32:04 +0800

    Add files via upload

---
+++ P2_1.java
@@ -0,0 +1,7 @@
+public class P2_1 {
+    
+public static void main(String[] args) {
+    System.err.println("Welcome to AD2204 Introduction to Programming");
+    System.err.println("This is Practical 2");
+}
+}
