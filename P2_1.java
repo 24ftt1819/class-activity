f7ad8c4a6d7debbfd69f8cd05315c3365781739f
24ftt1819 <24FTT1819@student.pb.edu.bn>
2025-08-04 21:44:32 +0800

    Delete P2_1.java

--- P2_1.java
+++
@@ -1,7 +0,0 @@
-public class P2_1 {
-    
-public static void main(String[] args) {
-    System.err.println("Welcome to AD2204 Introduction to Programming");
-    System.err.println("This is Practical 2");
-}
-}
